f2a6d4ed1d791a6975e49c25d35326fbb47ec75c
Stas Malolepszy <stas@mozilla.com>
2012-08-10 02:41:59 -0700

    update paths in generate_json.sh

--- generate_json.sh
+++ generate_json.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 HERE=$(cd `dirname $0`; pwd)
-LOLS=$HERE/lol
 L20N=$HOME/moz/code/l20n
 
 source $L20N/@l20n/bin/activate
 
-DUMP=$L20N/l20n/python/tools/dump_lol.py
+DUMP=$L20N/py/python/tools/dump_lol.py
 OPTS="-t json"
 
-for LOL in $(ls -1 $LOLS/*.lol);
+for LOL in $(ls -1 $HERE/*.lol);
 do
     NAME=$(basename $LOL .lol);
-    python $DUMP $OPTS $LOL > $LOLS/${NAME}.json
+    python $DUMP $OPTS $LOL > $HERE/${NAME}.json
 done
