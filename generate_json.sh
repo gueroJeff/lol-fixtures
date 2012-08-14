ab58d82959ea42ab252752c5dc5861412b7c0927
Stas Malolepszy <stas@mozilla.com>
2012-08-14 20:10:17 +0200

    change the path to dump_lol.py

--- generate_json.sh
+++ generate_json.sh
@@ -5,7 +5,7 @@ L20N=$HOME/moz/code/l20n
 
 source $L20N/@l20n/bin/activate
 
-DUMP=$L20N/py/python/tools/dump_lol.py
+DUMP=$L20N/py/tools/dump_lol.py
 OPTS="-t json"
 
 for LOL in $(ls -1 $HERE/*.lol);
